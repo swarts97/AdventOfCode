1337f2f5e4e3bf8169b678e2de88494120f07db5
szoke <szoke.tibor@eing.tigra.hu>
2022-12-05 20:22:26 +0100

    Day04

---
+++ 2022/Day04/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("2022/Day04/input.txt"));
+            String line;
+            int fullyContainedPairCounter = 0;
+
+            while ((line = reader.readLine()) != null) {
+                if (!line.isBlank()) {
+                    String[] rangesOfPair = line.split(",");
+                    int firstElfIDFrom = Integer.parseInt(rangesOfPair[0].split("-")[0]);
+                    int firstElfIDTo = Integer.parseInt(rangesOfPair[0].split("-")[1]);
+                    int secondElfIDFrom = Integer.parseInt(rangesOfPair[1].split("-")[0]);
+                    int secondElfIDTo = Integer.parseInt(rangesOfPair[1].split("-")[1]);
+
+                    if (areIDsPartiallyContained(firstElfIDFrom, firstElfIDTo, secondElfIDFrom, secondElfIDTo)) {
+                        fullyContainedPairCounter++;
+                    }
+                }
+            }
+            reader.close();
+            processResult(fullyContainedPairCounter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean areIDsFullyContained(int firstFrom, int firstTo, int secondFrom, int secondTo) {
+        return (firstFrom <= secondFrom && secondTo <= firstTo) ||
+                (secondFrom <= firstFrom && firstTo <= secondTo);
+    }
+
+    private static boolean areIDsPartiallyContained(int firstFrom, int firstTo, int secondFrom, int secondTo) {
+        return !(firstTo < secondFrom || secondTo < firstFrom);
+    }
+
+    private static void processResult(int highestCaloriesCount) throws IOException {
+        File outputFile = new File("2022/Day04/output.txt");
+        outputFile.createNewFile();
+        FileWriter myWriter = new FileWriter(outputFile);
+        myWriter.write(String.valueOf(highestCaloriesCount));
+        System.out.println(highestCaloriesCount);
+        myWriter.close();
+    }
+}
