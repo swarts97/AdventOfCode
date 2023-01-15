5eda1a0ab6d5a4beaa74e8b55f3c2b61d3f6e74e
swarts97 <szoke.tibi97@gmail.com>
2023-01-15 14:31:26 +0100

    Refactor for part two

--- 2022/Day24/src/Main.java
+++ 2022/Day24/src/Main.java
@@ -1,7 +1,5 @@
 import java.awt.*;
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     static final int WIDTH = 152; //152
@@ -10,59 +8,17 @@ public class Main {
     public static void main(String[] args) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader("2022/Day24/input.txt"));
-
             FieldCalculator fieldCalculator = new FieldCalculator(WIDTH, HEIGHT);
             fieldCalculator.initMtx(reader);
             Character[][][] mtx = fieldCalculator.getMtx();
 
-            Point goal = new Point(WIDTH - 2, HEIGHT - 2);
-            Set<Fieldv2> lastMinuteFields = new HashSet<>();
-            Fieldv2 field = new Fieldv2();
-            lastMinuteFields.add(field);
-            int result = 0;
-
-            for (int time = 1; time < MAXTRY; time++) {
-                Set<Fieldv2> nextIterationFields = new HashSet<>();
-                for (Fieldv2 lastMinuteField : lastMinuteFields) {
-                    nextIterationFields.addAll(lastMinuteField.getNextIterationFields(mtx[time]));
-                }
-                lastMinuteFields = nextIterationFields;
-                lastMinuteFields.removeIf(lastMinuteField -> headingTooSlow(goal, lastMinuteField));
-                for (Fieldv2 lmf : lastMinuteFields) {
-                    System.out.println("DistFromGoal: " + getDistanceFromGoal(goal, lmf.getMyPosition()) + " minutesPassed: " + lmf.getMinutesPassed());
-                }
-                if (lastMinuteFields.stream().anyMatch(fieldv2 -> goal.equals(fieldv2.getMyPosition()))) {
-                    Fieldv2 finalField = lastMinuteFields.stream()
-                            .filter(fieldv2 -> goal.equals(fieldv2.getMyPosition()))
-                            .findFirst().get();
-                    result = finalField.getMinutesPassed() + 1;
-                    System.out.println("================================");
-                    System.out.println("Solution found: " + result);
-                    System.out.println("================================");
-                    break;
-                }
-            }
+            Point firstGoal = new Point(WIDTH - 2, HEIGHT - 2);
+            PathFinder pathFinder = new PathFinder(HEIGHT, WIDTH, MAXTRY, 400, 30, firstGoal);
+            int result = pathFinder.getResult(mtx, 0);
             processResult(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    private static boolean headingTooSlow(Point goal, Fieldv2 field) {
-        int totalDistance = WIDTH - 3 + HEIGHT - 3 + 1;
-        int distanceFromGoal = getDistanceFromGoal(goal, field.getMyPosition());
-        int distanceFromStart = totalDistance - distanceFromGoal;
-        int drawbackFromIdeal = field.getMinutesPassed() - distanceFromStart;
-        //Heuristics from testinput calculations
-        int failureGap = 200;
-        int maximumAllowedPathLength = totalDistance + failureGap;
-
-        int acceptableDrawbackMinutes = failureGap * field.getMinutesPassed() / maximumAllowedPathLength + 30;
-        return drawbackFromIdeal > acceptableDrawbackMinutes;
-    }
-
-    private static int getDistanceFromGoal(Point goal, Point myPosition) {
-        return goal.x - myPosition.x + goal.y - myPosition.y;
     }
 
     private static void processResult(int result) throws IOException {
---
+++ 2022/Day24/src/PathFinder.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PathFinder {
+    private final int HEIGHT; //22
+    private final int WIDTH; //152
+    private final int MAXTRY; //400
+    private final int FAILURE_GAP; //400
+    private final int FAILURE_OFFSET; //20
+    private final Point goal;
+
+    public PathFinder(int HEIGHT, int WIDTH, int MAXTRY, int FAILURE_GAP, int FAILURE_OFFSET, Point goal) {
+        this.HEIGHT = HEIGHT;
+        this.WIDTH = WIDTH;
+        this.MAXTRY = MAXTRY;
+        this.FAILURE_GAP = FAILURE_GAP;
+        this.FAILURE_OFFSET = FAILURE_OFFSET;
+        this.goal = goal;
+    }
+
+    public int getResult(Character[][][] mtx, int timePassed) {
+        Set<Fieldv2> lastMinuteFields = new HashSet<>();
+        Fieldv2 initialField = new Fieldv2();
+        lastMinuteFields.add(initialField);
+
+        for (int time = timePassed + 1; time < timePassed + MAXTRY; time++) {
+            Set<Fieldv2> nextIterationFields = new HashSet<>();
+            for (Fieldv2 lastMinuteField : lastMinuteFields) {
+                nextIterationFields.addAll(lastMinuteField.getNextIterationFields(mtx[time]));
+            }
+            lastMinuteFields = nextIterationFields;
+            lastMinuteFields.removeIf(lastMinuteField -> headingTooSlow(goal, lastMinuteField));
+            for (Fieldv2 lmf : lastMinuteFields) {
+                System.out.println("DistFromGoal: " + getDistanceFromGoal(goal, lmf.getMyPosition()) + " minutesPassed: " + lmf.getMinutesPassed());
+            }
+            if (lastMinuteFields.stream().anyMatch(fieldv2 -> goal.equals(fieldv2.getMyPosition()))) {
+                Fieldv2 finalField = lastMinuteFields.stream()
+                        .filter(fieldv2 -> goal.equals(fieldv2.getMyPosition()))
+                        .findFirst().get();
+                int result = finalField.getMinutesPassed() + 1;
+                System.out.println("================================");
+                System.out.println("Solution found: " + result);
+                System.out.println("================================");
+                return result;
+            }
+        }
+        return -1;
+    }
+
+    private boolean headingTooSlow(Point goal, Fieldv2 field) {
+        int totalDistance = WIDTH - 3 + HEIGHT - 3 + 1;
+        int distanceFromGoal = getDistanceFromGoal(goal, field.getMyPosition());
+        int distanceFromStart = totalDistance - distanceFromGoal;
+        int drawbackFromIdeal = field.getMinutesPassed() - distanceFromStart;
+        //Heuristics from testinput calculations
+        int failureGap = FAILURE_GAP;
+        int maximumAllowedPathLength = totalDistance + failureGap;
+
+        int acceptableDrawbackMinutes = failureGap * field.getMinutesPassed() / maximumAllowedPathLength + FAILURE_OFFSET;
+        return drawbackFromIdeal > acceptableDrawbackMinutes;
+    }
+
+    private int getDistanceFromGoal(Point goal, Point myPosition) {
+        return goal.x - myPosition.x + goal.y - myPosition.y;
+    }
+}
